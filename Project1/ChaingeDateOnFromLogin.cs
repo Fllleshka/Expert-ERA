8b36165660b8b94ea93f448e7960583dd2faba6b
Владислав <fleisner.omsk@gmail.com>
2019-10-29 09:31:45 +0300

    Изменена форма "Изменение данных для формы входа". Добавлено добавление нового пользователя. Необходимо добавить обновление DataGridView при добавлении нового пользователя.

--- Project1/ChaingeDateOnFromLogin.cs
+++ Project1/ChaingeDateOnFromLogin.cs
@@ -64,5 +64,44 @@ namespace Project1
             npgSqlConnection.Close();
 
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Добавление нового пользователя в систему
+            // Инициализация переменных
+            string Surname_textbox1 = textBox1.Text;
+            string Name_textbox2 = textBox2.Text;
+            string Middlename_textbox3 = textBox3.Text;
+
+            // Тут должна быть проверка на корректность входящих значений
+            // Нужно ли тут это? Или понадеятся что администратор всё будет вводить корректно.
+            //Chainge_textbox(Surname_textbox1);
+            //Chainge_textbox(Name_textbox2);
+            //Chainge_textbox(Middlename_textbox3);
+
+            // Строка для подключения к БД.
+            String connectionString = "Server=localhost;Port=5432;User Id=postgres;Password=1q2w3e4r;Database=Expert_ERA;";
+
+            // Подключаем Базу Данных PostgreSQL через Npgsql
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+            npgSqlConnection.Open();
+
+            // Строка запроса для определения последнего номера в таблице, для формирования запроса к БД
+            string text_max_id_fio = "SELECT MAX(ID_FIO) FROM FIO";
+            NpgsqlCommand npgSqlCommand = new NpgsqlCommand(text_max_id_fio, npgSqlConnection);
+            // Записываем ответ в переменную.
+            string result_max_id_fio = npgSqlCommand.ExecuteScalar().ToString();
+            // Преобразовываем переменную в int а так же прибавляем 1 для продолжения нумерации. Про автоинкримент ничего не знаю!Ы
+            int result = Convert.ToInt32(result_max_id_fio) + 1;
+
+            // Строка запроса для вставки в БД данных с формы
+            string insert_into_FIO = "INSERT INTO FIO (Id_fio, First_name, Second_name, Surname) VALUES('" + result + "', '" + Surname_textbox1 + "', '" + Name_textbox2 + "', '" + Middlename_textbox3 + "');";
+            npgSqlCommand = new NpgsqlCommand(insert_into_FIO, npgSqlConnection);
+            result_max_id_fio = npgSqlCommand.ExecuteNonQuery().ToString();
+            MessageBox.Show(result_max_id_fio);
+
+            // Обновить таблицу DataGridView
+
+        }
     }
 }
